bde91b0bb4bad0f1f2a1dac835ccc276ec16d524
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-03-15 22:17:29 -0300

    feat: Finalizado carrinho

--- aulas/products-house-v2/src/controllers/CarrinhoController.js
+++ aulas/products-house-v2/src/controllers/CarrinhoController.js
@@ -1,6 +1,47 @@
 const CarrinhoController = {
     show: (req, res) => {
-        return res.render('carrinho');
+        const { carrinho } = req.session.user;
+
+        let total = 0;
+
+        if (!carrinho) {
+            return res.render('carrinho', { carrinho: [], total });
+        }
+
+        carrinho.forEach(produto => {
+            total += Number(produto.price);
+        });
+
+        return res.render('carrinho', { carrinho, total });
+    },
+
+    addCart: (req, res) => {
+        const { id, name, price, image } = req.body;
+        const produto = { id, name, price, image };
+
+        if (req.session.user.carrinho) {
+            req.session.user.carrinho.push(produto)
+        } else {
+            req.session.user.carrinho = [produto];
+        }
+
+        return res.redirect('/carrinho');
+    },
+
+    removeCart: (req, res) => {
+        const { id } = req.params;
+        let { carrinho } = req.session.user;
+
+        const index = carrinho.findIndex(produto => produto.id == id);
+        const carrinhoAtualizado = carrinho.splice(index, 1);
+        carrinho = carrinhoAtualizado;
+
+        if (carrinho.length <= 0) {
+            carrinho = [];
+            return res.redirect('/carrinho');
+        }
+
+        return res.redirect('/carrinho');
     }
 };
 
--- aulas/products-house-v2/src/routes/carrinhoRouter.js
+++ aulas/products-house-v2/src/routes/carrinhoRouter.js
@@ -5,5 +5,7 @@ const isLogin = require('../middlewares/isLogin');
 
 router.use(isLogin);
 router.get('/carrinho', CarrinhoController.show);
+router.post('/carrinho/adicionar', CarrinhoController.addCart);
+router.delete('/carrinho/:id/deletar', CarrinhoController.removeCart);
 
 module.exports = router;
